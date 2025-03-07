a75dfc9a27b95ca831ad8d44b30c2201e9b51856
Sheetal Naik <sheetalnaik848@gmail.com>
2025-03-07 17:21:30 +0530

    Add files via upload

---
+++ clicker app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Clicker from './components/Clicker'
+
+function App() {
+
+  return (
+    <>
+      <Header title='Clicker' />
+      <Clicker />
+    </>
+  )
+}
+
+export default App
