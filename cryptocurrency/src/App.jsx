6bc648622425ecb3686b9c76a8921f35c3054e8e
Sheetal Naik <sheetalnaik848@gmail.com>
2025-03-10 16:27:52 +0530

    Add files via upload

---
+++ cryptocurrency/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CryptoCurrencyInfo from './components/CryptoCurrencyInfo'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <CryptoCurrencyInfo />
+    </>
+  )
+}
+
+export default App
---
+++ cryptocurrency/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="Header">
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="w-100 text-light">
+                <h3>CryptoCurrency Information App</h3>
+            </div>
+        </nav>
+    </div>
+    </>
+  )
+}
+
+export default Header
